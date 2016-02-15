14b377110fcac7dab1c9dd164eefcdf448941476
Saleh1204 <stu2@rlk20.pc.ccse.kfupm.edu.sa>
2016-02-15 09:36:26 +0300

    Added An Empty Sequential Project

---
+++ Sequential/sequential.c
@@ -0,0 +1,17 @@
+/*
+ * sequential.c
+ *
+ *  Created on: Feb 15, 2016
+ *      Author: stu2
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main()
+{
+
+
+	printf("Testing C Program with Nsight\n");
+	return 0;
+}
